68e5c1dd67ca969ea562033f6ca2cd6fa9aaee03
Allen_Yeh <happy1294123@gmail.com>
2023-12-21 07:58:35 +0800

    config: eslint format

--- src/router/index.js
+++ src/router/index.js
@@ -1,37 +1,41 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import Home from '../views/Home.vue'
+import { createRouter, createWebHistory } from 'vue-router';
+import Home from '../views/Home.vue';
 
 const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home,
-  },
-  {
-    path: '/admin/membergroup',
-    name: 'AdminMemberGroup',
-    component: () => import('../views/admin/AdminMemberGroup.vue'),
-  },
-  {
-    path: '/admin/membermanage/:memberId',
-    name: 'AdminMember',
-    component: () => import('../views/admin/AdminMember.vue'),
-  },
-  {
-    path: '/admin/membermanage',
-    name: 'AdminMemberManage',
-    component: () => import('../views/admin/AdminMemberManage.vue'),
-  },
-  {
-    path: '/:catchAll(.*)',
-    name: 'not-found',
-    component: () => import('../views/NotFound.vue')
-  }
-]
+	{
+		path: '/',
+		name: 'Home',
+		component: Home,
+	},
+	{
+		path: '/admin/membergroup',
+		name: 'AdminMemberGroup',
+		component: () => import('../views/admin/AdminMemberGroup.vue'),
+	},
+	{
+		path: '/admin/membermanage/:memberId',
+		name: 'AdminMember',
+		component: () => import('../views/admin/AdminMember.vue'),
+	},
+	{
+		path: '/admin/membermanage',
+		name: 'AdminMemberManage',
+		component: () => import('../views/admin/AdminMemberManage.vue'),
+	},
+	{
+		path: '/admin',
+		redirect: { name: 'AdminMemberGroup' }
+	},
+	{
+		path: '/:catchAll(.*)',
+		name: 'not-found',
+		component: () => import('../views/NotFound.vue')
+	}
+];
 
 const router = createRouter({
-  history: createWebHistory(),
-  routes,
-})
+	history: createWebHistory(),
+	routes,
+});
 
-export default router
+export default router;
