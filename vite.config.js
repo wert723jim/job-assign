47a05c35bcaee6b17205f90653b31a52f1df0578
LAPTOP-DHHLQT9N\wert7 <wert723jim@gmail.com>
2023-12-21 16:32:37 +0800

    modify: eslint coding style change

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
-import { defineConfig } from 'vite';
-import vue from '@vitejs/plugin-vue';
-import eslintPlugin from 'vite-plugin-eslint';
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import eslintPlugin from 'vite-plugin-eslint'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -17,4 +17,4 @@ export default defineConfig({
 			'@': '/src',
 		},
 	},
-});
+})
