4cfae73c2a7f10bc740c6823b3e0e7c17c0728f9
Allen_Yeh <happy1294123@gmail.com>
2023-12-23 03:35:54 +0800

    feat: mobile side bar

--- src/main.js
+++ src/main.js
@@ -2,5 +2,8 @@ import { createApp } from 'vue'
 import './assets/style.css'
 import App from './App.vue'
 import router from './router'
+import 'vue-sidebar-menu/dist/vue-sidebar-menu.css'
 
-createApp(App).use(router).mount('#app')
+createApp(App)
+    .use(router)
+    .mount('#app')
